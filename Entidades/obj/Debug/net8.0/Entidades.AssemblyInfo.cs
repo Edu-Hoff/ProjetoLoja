e3f4ec7ab7be0123a80c31987ff0c43baa47b8ce
Edu-Hoff <eduardohoffmanndaveiga@gmail.com>
2025-05-13 20:06:57 -0300

    Teste

--- Entidades/obj/Debug/net8.0/Entidades.AssemblyInfo.cs
+++ Entidades/obj/Debug/net8.0/Entidades.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entidades")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49a97acbb9f7acba89f5927521908164c6263b08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5665a8a930ad3551f0849854241c7aea498cdac4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entidades")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entidades")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
