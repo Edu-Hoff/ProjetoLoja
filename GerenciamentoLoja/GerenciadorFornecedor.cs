df0b5df082bd5074f94fb7e2f858308cae302e0c
Luana Laskoski <luanalaskoski7@gmail.com>
2025-05-18 11:08:34 -0300

    Alteração GerenciadorFornecedor

--- GerenciamentoLoja/GerenciadorFornecedor.cs
+++ GerenciamentoLoja/GerenciadorFornecedor.cs
@@ -1,30 +1,67 @@
 using System;
+using System.Data.Common;
 using Entidades;
 
 namespace GerenciamentoLoja;
 
 public class GerenciadorFornecedor
 {
-    Fornecedor[] todosFornecedores = new Fornecedor[1];
+    Fornecedor[] todosFornecedores = new Fornecedor[5]; //vetor inicial
     public void CadastraFornecedor()
     {
         Console.WriteLine("Id do fornecedor: ");
-        String Id = Console.ReadLine();
+        Id = Console.ReadLine();
         Console.WriteLine("Nome do fornecedor: ");
-        String Nome = Console.ReadLine();
+        Nome = Console.ReadLine();
         Console.WriteLine("Descrição do fornecedor: ");
-        String Descricao = Console.ReadLine();
+        Descricao = Console.ReadLine();
         Console.WriteLine("Telefone do fornecedor: ");
-        String Telefone = Console.ReadLine();
+        Telefone = Console.ReadLine();
         Console.WriteLine("Email do fornecedor: ");
-        String Email = Console.ReadLine();
+        Email = Console.ReadLine();
+
+        Fornecedor[] novoVetorFornecedores = new Fornecedor[todosFornecedores.Length + 1];
+        for (int i = 0; i < todosFornecedores.Length; i++)
+        {
+            novoVetorFornecedores[i] = todosFornecedores[i];
+        }
+        novoVetorFornecedores[novoVetorFornecedores.Length - 1] = new Fornecedor(Id, Nome, Descricao, Telefone, Email);
+        todosFornecedores = novoVetorFornecedores;
     }
     public void AlteraFornecedor()
     {
-        
+        Console.Write("Informe o Id do fornecedor:\n");
+        String IdCodigo = Console.ReadLine();
+
+        for (int i = 0; i < todosFornecedores.Length; i++)
+        {
+            if (todosFornecedores[i].Id == IdCodigo) //se os dois códigos forem iguais, altera as informações de tal código
+            {
+                Console.Write("Novo nome do fornecedor:\n");
+                todosFornecedores[i].Nome = Console.ReadLine();
+                Console.Write("Nova descrição do fornecedor:\n");
+                todosFornecedores[i].Descricao = Console.ReadLine();
+                Console.Write("Novo telefone do fornecedor:\n");
+                todosFornecedores[i].Telefone = Console.ReadLine();
+                Console.Write("Novo email do fornecedor:\n");
+                todosFornecedores[i].Email = Console.ReadLine();
+
+                Console.WriteLine("Fornecedor alterado com sucesso!\n");
+            }
+            Console.WriteLine("Fornecedor não encontrado!\n");
+        }
     }
     public void ExcluiFornecedor()
     {
-        
+        Console.Write("Informe o Id do fornecedor:\n");
+        String IdCodigo = Console.ReadLine();
+
+        for (int i = 0; i < todosFornecedores.Length; i++)
+        {
+            if (todosFornecedores[i].Id == IdCodigo)
+            {
+                
+            }
+        }
     }
 }
